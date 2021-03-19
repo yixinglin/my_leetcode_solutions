c3eadefb0b9daac037b4039e12e053e3622f6a50
Yixing Lin <184059914@qq.com>
2021-03-19 21:54:36 +0100

    LeetCode 0004, Binary search with O(log m)

--- leetcode 0004/main.cpp
+++ leetcode 0004/main.cpp
@@ -35,6 +35,46 @@ public:
             return sorted_nums[i];
         }
     }
+
+    // Method 2, O(log m)
+    double method2(vector<int>& nums1, vector<int>& nums2) {
+
+        int LenA = nums1.size(), LenB = nums2.size();
+        int Len = LenA + LenB;
+
+        if(LenA > LenB) {
+            swap(nums1, nums2); swap(LenA, LenB);
+        }
+
+        if(LenA == 0)
+            return (nums2[(LenB-1) / 2] + nums2[LenB / 2]) / 2.0;
+
+        int startA = 0, endA = LenA;
+        int splitA, splitB, L1, L2, R1, R2;
+        while(startA <= endA) {
+            splitA = (startA + endA) /2;
+            splitB = (Len+1) / 2 - splitA;
+            L1 = splitA == 0? INT_MIN: nums1[splitA-1];
+            R1 = splitA == LenA? INT_MAX: nums1[splitA];
+            L2 = splitB == 0? INT_MIN: nums2[splitB -1 ];
+            R2 = splitB == LenB? INT_MAX: nums2[splitB];
+            if(L1 > R2)
+                endA = splitA -1;
+            else if (L2 > R1)
+                startA = splitA + 1;
+            else {
+                if (Len % 2 == 0)
+                    return (max(L1, L2) + min(R1, R2)) / 2.0;
+                else
+                    return max(L1, L2);
+            }
+        }
+
+        return 0;
+    }
+
+
+
 };
 
 
